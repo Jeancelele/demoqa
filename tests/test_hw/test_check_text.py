462fa56078e701d818dd68d975d5ce8b5ee18648
Jeancelele <IvanKy2311@yandex.ru>
2024-11-24 11:29:27 +0300

    Add files via upload

---
+++ tests/test_hw/test_check_text.py
@@ -0,0 +1,16 @@
+#from pages.demoqa import Demoqa
+#from pages.elements_page import ElementsPage
+
+
+#def test_check_footer(browser):
+    #demo_qa_page = Demoqa(browser)
+    #demo_qa_page.visit()
+    #assert demo_qa_page.text_footer.get_text() == '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.'
+
+#def test_check_text_place(browser):
+    #demo_qa_page = Demoqa(browser)
+   # el_page = ElementsPage(browser)
+
+    #demo_qa_page.visit()
+    #demo_qa_page.btn_elements.click()
+    #assert el_page.text_place.get_text() == 'Please select an item from left to start practice.'
